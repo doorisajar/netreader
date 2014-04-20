2aa3cf55c4f4121bc22bdc783b23f7a8e5d938be
Raja Doake <rdoake@gmail.com>
2014-04-20 17:10:49 -0500

    Moved data pack computations from matchups.R to octgn.R so that all other scripts can rely on the presence of the Pack column.

--- octgn.R
+++ octgn.R
@@ -38,12 +38,14 @@ octgn.df <- mutate(octgn.df, Win = (Result == "AgendaVictory"   |
                                     )
                    )
 
-# May not need this anymore with the conversion to dplyr. 
-# octgn.df$Win <- as.factor(octgn.df$Win)
-
 # Rename the columns to simpler values. 
 names(octgn.df)[names(octgn.df) == "Corporation"] <- "CorpID"
 names(octgn.df)[names(octgn.df) == "Runner"] <- "RunID"
+
+
+#-----------------------------------------------------------------------------
+# FILTERING
+#-----------------------------------------------------------------------------
 
 # Filter out Laramy Fisk, The Collective, wins/losses by concession, and deck sizes of zero. 
 octgn.df <- filter(octgn.df, RunID              != "Criminal | Laramy Fisk"                &
@@ -56,3 +58,92 @@ octgn.df <- filter(octgn.df, RunID              != "Criminal | Laramy Fisk"     
                              Corp_Deck_Size     >= 40                                      &
                              Duration           >= 0
 )
+
+
+#-----------------------------------------------------------------------------
+# DATA PACKS
+#-----------------------------------------------------------------------------
+
+# Use version number to determine which data packs were included. 
+
+# List of plugin versions associated with data packs, for reference. 
+# "3.9.0","Double Time"
+# "3.8.0","Fear and Loathing"
+# "3.7.0","True Colors"
+# "3.6.0","Mala Tempora"
+# "3.5.0","Second Thoughts"
+# "3.3.0","Opening Moves"
+# "3.2.0","Creation and Control"
+# "3.1.0","Future Proof"
+# "3.0.0","Humanity's Shadow"
+# "2.3.0","A Study in Static"
+# "2.2.0","Cyber Exodus"
+# "2.1.0","Trace Amount" # NOT CERTAIN
+# "2.0.0","What Lies Ahead"
+
+# Consolidate all packs into a df. 
+data.packs <- data.frame(rep( (20:39) / 10 ), 
+                         c("What Lies Ahead",
+                           "Trace Amount",
+                           "Cyber Exodus",
+                           "A Study in Static",
+                           "A Study in Static",
+                           "A Study in Static",
+                           "A Study in Static",
+                           "A Study in Static",
+                           "A Study in Static",
+                           "A Study in Static",
+                           "Humanity's Shadow",
+                           "Future Proof",
+                           "Creation and Control",
+                           "Opening Moves",
+                           "Opening Moves",
+                           "Second Thoughts",
+                           "Mala Tempora",
+                           "True Colors",
+                           "Fear and Loathing",
+                           "Double Time"))
+
+names(data.packs) <- c("Version", "Pack")
+
+data.packs$Pack <- ordered(data.packs$Pack, c("What Lies Ahead",
+                                              "Trace Amount",
+                                              "Cyber Exodus",
+                                              "A Study in Static",
+                                              "Humanity's Shadow",
+                                              "Future Proof",
+                                              "Creation and Control",
+                                              "Opening Moves",
+                                              "Second Thoughts",
+                                              "Mala Tempora",
+                                              "True Colors",
+                                              "Fear and Loathing",
+                                              "Double Time"
+)
+)
+
+data.packs <- select(data.packs, Version:Pack)
+data.packs$Version <- as.character(data.packs$Version)
+data.packs$Version[1] <- "2.0"
+data.packs$Version[11] <- "3.0"
+
+# Need to match the versions in this df to the versions in octgn.df. 
+# Follow this form:
+#
+# substr(octgn.df$Version[1], 1, 3)
+# 
+# For each 1:3 chars in octgn.df$Version, I want to match with data.packs$Version and populate a new
+# column in octgn.df with the associated data pack. 
+#
+# Write a simple match function to take a character string of the form "x.x" and return the most recent
+# data pack. 
+
+CheckPack <- function(x, data.packs) {
+  return( data.packs$Pack[match( substr(x, 1, 3), data.packs$Version )] )
+}
+
+octgn.df <- mutate(octgn.df, Pack = CheckPack(octgn.df$Version, data.packs))
+
+# I checked out the games with Pack = NA; they're simply a small subset of games (about 300) where
+# the version number was either mangled or missing. No problem to simply drop them. 
+octgn.df <- filter(octgn.df, Pack %in% levels(octgn.df$Pack))
